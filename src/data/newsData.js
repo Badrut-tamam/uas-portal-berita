c9be0dc1586fe9b2649bbdc24a0a301360d452d9
Badrut-tamam <Tamambadrut712@gmail.com>
2026-01-03 16:57:51 +0700

    perbaiki url gambar

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -3,42 +3,42 @@ const newsData = [
         id: 1,
         title: "Penerapan AI di Smart City Jakarta",
         description: "Pemerintah DKI Jakarta mulai mengintegrasikan kecerdasan buatan untuk mengurai kemacetan dan optimalisasi lampu merah di bebagai ruas jalan protokol.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Jakarta_Skyline_Part_2.jpg/640px-Jakarta_Skyline_Part_2.jpg",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/JakartaSkyline.jpg?width=600",
         date: "2024-01-03"
     },
     {
         id: 2,
         title: "Satelit SATRIA-1 Siap Beroperasi",
         description: "Satelit Republik Indonesia (SATRIA-1) sukses mencapai orbit dan siap meratakan akses internet di daerah 3T (Tertinggal, Terdepan, Terluar).",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Falcon_9_launch_Telstar_18_Vantage_%28cropped%29.jpg/640px-Falcon_9_launch_Telstar_18_Vantage_%28cropped%29.jpg",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/Satelit_Satria_1_Indonesia.jpg?width=600",
         date: "2024-01-02"
     },
     {
         id: 3,
         title: "Timnas Indonesia vs Jepang",
         description: "Skuad Garuda bersiap menghadapi laga krusial di Stadion Utama Gelora Bung Karno dengan dukungan penuh suporter tanah air.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Gelora_Bung_Karno_Stadium_inside.jpg/640px-Gelora_Bung_Karno_Stadium_inside.jpg",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gelora_Bung_Karno_Stadium.jpg?width=600",
         date: "2023-12-28"
     },
     {
         id: 4,
         title: "Mobil Listrik Buatan Anak Bangsa",
         description: "Produsen otomotif lokal mulai unjuk gigi memamerkan prototipe mobil listrik hemat energi yang siap bersaing di pasar global.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/G20_Bali_Summit_Electronic_Vehicle_Showcase_%282%29.jpg/640px-G20_Bali_Summit_Electronic_Vehicle_Showcase_%282%29.jpg",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/Wuling_Air_EV_(Rear),_Jakarta,_Indonesia.jpg?width=600",
         date: "2023-12-25"
     },
     {
         id: 5,
         title: "Kuliner Rendang Mendunia",
         description: "Rendang kembali dinobatkan sebagai salah satu makanan terenak di dunia, memicu lonjakan ekspor bumbu rempah asli Minang.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Rendang_Padang_-_Padang_Dish.jpg/640px-Rendang_Padang_-_Padang_Dish.jpg",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/Rendang_daging_sapi_asli_Padang.JPG?width=600",
         date: "2023-12-20"
     },
     {
         id: 6,
         title: "Pesona Raja Ampat Papua",
         description: "Keindahan bawah laut Raja Ampat terus memikat wisatawan mancanegara, menjadikannya destinasi prioritas pariwisata Indonesia tahun ini.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Raja_Ampat_Islands_-_Journal.pbio.1001457.g001.png/640px-Raja_Ampat_Islands_-_Journal.pbio.1001457.g001.png",
+        image: "https://commons.wikimedia.org/wiki/Special:FilePath/Raja_Ampat,_West_Papua,_Indonesia.jpg?width=600",
         date: "2023-12-15"
     }
 ];
