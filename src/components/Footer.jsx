f265db16522a5a583067992d471a36028d40f66d
Badrut-tamam <Tamambadrut712@gmail.com>
2026-01-03 17:02:36 +0700

    ubah warna footer menjadi biru

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-4 mt-5">
+    <footer className="bg-primary text-white text-center py-4 mt-5">
       <Container>
         <p className="mb-1 fw-bold">Portal Berita</p>
         <p className="mb-0 small text-white-50">© 2024 Portal Berita of the Future. All Rights Reserved.</p>
